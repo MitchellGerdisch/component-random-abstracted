41f30e418b7f31910784662f0bf57ff617d82ac4
MitchellGerdisch <mitch@pulumi.com>
2025-03-05 11:39:36 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# component-ts
-Typescript component used in various Pulumi programs.
+# random-component-ts
+A simple Pulumi Component Resource (https://www.pulumi.com/docs/iac/concepts/resources/components/) that abstracts the use of the Random provider (https://www.pulumi.com/registry/packages/random/)
+
+It can be used for testing component resources and the "components as components" feature, etc.
---
+++ petAbstracted.ts
@@ -0,0 +1,39 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+
+export interface PetAbstractedArgs {
+    size: string;
+}
+
+// This resources helps you create a self signed certificate.
+export class PetAbstracted extends pulumi.ComponentResource {
+    // The PEM of your self signed cert.
+    public readonly petName: pulumi.Output<string>;
+
+    constructor(name: string, args: PetAbstractedArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("random-abstracted:index:PetAbstracted", name, args, opts);
+
+        const size = args.size || "small";
+
+        var petNameLength: number;
+        switch (size) {
+            case "small":
+                petNameLength = 2;
+                break;
+            case "medium":
+                petNameLength = 4;
+                break;
+            case "large":
+                petNameLength = 6;
+                break;
+            default:
+                throw new Error("Invalid size");
+        }
+
+        const petName = new random.RandomPet("petName", {
+            length: petNameLength,
+        }, { parent: this });
+
+        this.petName = petName.id
+    }
+}
