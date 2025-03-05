954e75a95360239964767dc8594f91ce3ed92cbc
MitchellGerdisch <mitch@pulumi.com>
2025-03-05 16:57:41 -0600

    Fix error message

--- petAbstracted.ts
+++ petAbstracted.ts
@@ -30,7 +30,7 @@ export class PetAbstracted extends pulumi.ComponentResource {
                 petNameLength = 8;
                 break;
             default:
-                throw new Error("Invalid size. Must be small, medium, large or extra-large.");
+                throw new Error("Invalid size. Must be small, medium, large or xlarge.");
         }
 
         const petName = new random.RandomPet("petName", {
